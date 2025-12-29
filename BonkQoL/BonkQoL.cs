30e1ff2d05e98b4cb0eb23b6fb52029b8af689e9
WhitePhant0m <kallunki.aleksi@gmail.com>
2025-12-29 21:18:09 +0200

    first feature

--- BonkQoL/BonkQoL.cs
+++ BonkQoL/BonkQoL.cs
@@ -1,16 +1,32 @@
-﻿using BepInEx;
+﻿using Assets.Scripts.Inventory__Items__Pickups.Pickups;
+using BepInEx;
 using BepInEx.Logging;
 using BepInEx.Unity.IL2CPP;
+using HarmonyLib;
 
-namespace BepInEx6.PluginTemplate.Unity.Il2Cpp;
+namespace BonkQoL;
 
 [BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
-public class Plugin : BasePlugin {
-    internal static new ManualLogSource Log;
+public class Plugin : BasePlugin
+{
+    internal new static ManualLogSource Log;
+    private static readonly Harmony Harmony = new Harmony(MyPluginInfo.PLUGIN_GUID);
 
-    public override void Load() {
+    public override void Load()
+    {
         // Plugin startup logic
         Log = base.Log;
         Log.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+
+        Harmony.PatchAll();
+    }
+
+    [HarmonyPatch(typeof(PickupManager), nameof(PickupManager.SpawnPickup))]
+    public class PickupPatch
+    {
+        public static void PostFix(EPickup pickup, PickupManager __instance, ref Pickup __result)
+        {
+            if (__result != null && pickup == EPickup.Xp) __result.ApplyPickup();
+        }
     }
 }
