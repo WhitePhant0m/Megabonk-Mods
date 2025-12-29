f06516ef8914996cf060e85dd205ff4eabddc05f
WhitePhant0m <kallunki.aleksi@gmail.com>
2025-12-29 20:31:41 +0200

    actual init

---
+++ BonkQoL/BonkQoL.cs
@@ -0,0 +1,16 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using BepInEx.Unity.IL2CPP;
+
+namespace BepInEx6.PluginTemplate.Unity.Il2Cpp;
+
+[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+public class Plugin : BasePlugin {
+    internal static new ManualLogSource Log;
+
+    public override void Load() {
+        // Plugin startup logic
+        Log = base.Log;
+        Log.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+    }
+}
